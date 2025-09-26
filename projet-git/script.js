c6264f8cd7a8db50ebb081c2487e9efcef6af6c0
khadija <khadijaceurrouj57@gmail.com>
2025-09-26 12:52:31 +0100

    CH 12 et 13

---
+++ projet-git/script.js
@@ -0,0 +1,42 @@
+// function addition(a, b) {
+//   return a + b;
+// }
+
+// let somme = addition(8, 5);
+// console.log("La somme est : " + somme);
+
+
+
+//   chaptre 9 
+//  let age = 20;
+
+// if (age < 18) {
+//   console.log("Vous êtes mineur.");
+// } else if (age === 18) {
+//   console.log("Vous venez d’atteindre la majorité !");
+// } else {
+//   console.log("Vous êtes majeur.");
+// }
+//chaptre 10
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 5) continue; // sauter le 5
+//   console.log("Nombre : " + i);
+// }
+//chaptre 7
+// let nombre1 = 10;
+// let nombre2 = 5;
+// let somme = nombre1 + nombre2;
+// console.log("La somme est : " + somme);
+//chaptre 12
+  let competence = ["c1","c3","c2","c1","c3","c8","c2","c1"];
+  let competeur ={};
+  for(let i=0; i<competences.length; i++){
+    let code=compteur[i];
+    if (competeur[code] ===undefined){
+      competeur[code]=1
+    }else{
+      compteurs[code]++;
+    }
+  }
+  console.log(compteurs)
+
